a9628891c6e266d4bf5ddbbc2e97c6398e9d5433
Lavinia-ant <lavinia.antunes@aluno.ifsp.edu.br>
2026-06-15 08:37:02 -0300

    Efetuando login

--- src/libs/jwt.ts
+++ src/libs/jwt.ts
@@ -2,7 +2,15 @@ import jwt from 'jsonwebtoken';
 
 export const createJWT = (payload: any) => {
     return jwt.sign(
-        payload, 
+        payload,
         process.env.JWT_SECRET_KEY as string
     )
+}
+
+export const verifyJWT = (token: string) => {
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+    } catch (error) {
+        return false
+    }
 }
